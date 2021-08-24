02ee64bebc555b6214cb7ec4f8a16452980d9e13
igordmf <igordanielmf@gmail.com>
2021-08-24 14:09:34 -0300

    Arquivo do rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-/* import loginReducer from './loginReducer'; */
+import musicsReducer from './musicsReducer';
+import favoritesReducer from './favoritesReducer';
 
 const rootReducer = combineReducers({
-  /* loginReducer, */
+  musicsReducer,
+  favoritesReducer,
 });
 
 export default rootReducer;
