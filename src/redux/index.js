65c158f2a406626f17ddeae0d731ad1784d84ff1
igordmf <igordanielmf@gmail.com>
2021-08-24 14:08:18 -0300

    Arquivo store redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
